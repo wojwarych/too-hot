e8c0692d4fe1503a01d6faac8ba76e97aa152587
Wojciech Warych <w.warych@xberry.tech>
2021-04-04 14:43:31 +0200

    added basic integration with mongodb + endpoint for list of sequences and adding new sequence

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 
+from db import db
+from models import Sequence
+
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/sequences/")
+async def list_sequences():
+    sequences = []
+    for seq in db.sequences.find():
+        sequences.append(Sequence(**seq))
+    return sequences
+
+
+@app.post("/sequences/", status_code=201)
+async def insert_sequence(sequence: Sequence):
+    if hasattr(sequence, "id"):
+        delattr(sequence, "id")
+    ret = db.sequences.insert_one(sequence.dict(by_alias=True))
+    return sequence.dict()
